ce848f0a26e650a307454ebd41a20e92ee422d2d
PANFACTORY <yutatanabe510@gmail.com>
2020-11-03 11:55:10 +0900

    Add comments

--- client.cpp
+++ client.cpp
@@ -1,52 +1,47 @@
 //  Client side
-#include <stdio.h>
+#include <iostream>
 #include <sys/types.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
 int main(int argc, char *argv[]) {
-    WSADATA wsaData;
-    struct sockaddr_in server;
- 
-    char buf[32];
-    char *deststr;
-    unsigned int **addrptr;
-
-    //  (0) Initialize 
+    //  (0-A) アドレス取得，コマンドライン引数に渡すこと 
     if (argc != 2) {
-        printf("Usage : %s dest\n", argv[0]);
+        std::cout << "Usage : " << argv[0] << " dest" << std::endl;
         return 1;
     }
-    deststr = argv[1];
+    std::string deststr(argv[1]);
  
+    //  (0-B) 初期化
+    WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2,0), &wsaData) == SOCKET_ERROR) {
-        printf ("Error initialising WSA.\n");
+        std::cout << "Error initialising WSA." << std::endl;
         return -1;
     }
 
+    //  (1) ソケットの作成
     SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("socket");
         return 1;
     }
 
-    //  (1)
+    //  (2) サーバーと接続
+    struct sockaddr_in server;
     server.sin_family = AF_INET;
     server.sin_port = htons(50000);
-    server.sin_addr.s_addr = inet_addr(deststr);
-
-    //  (2)connect
-    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) != 0) {
+    server.sin_addr.s_addr = inet_addr(deststr.c_str());
+    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) != 0) {
         perror("connect");
         return 1;
     }
 
-    //  (3)
-    char vect[512]={0};
-    int get=recv(sock, vect, 512, 0);
-    printf("read=%s\n", vect);
+    //  (3) サーバーからメッセージを受け取る
+    char vect[512] = { 0 };
+    int get = recv(sock, vect, 512, 0);
+    std::cout << "read=" << vect << std::endl;
  
-    //  (4) Terminate 
+    //  (4) 切断
     closesocket(sock);
     WSACleanup();
 
--- server.cpp
+++ server.cpp
@@ -6,18 +6,15 @@
 #include <errno.h>
 
 int main() {
-    WSADATA wsaData;    
-    struct sockaddr_in addr;
-    struct sockaddr_in client;
-    
-    //  (0) Initialize
+    //  (0) 初期化
+    WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2 ,0), &wsaData) == SOCKET_ERROR) {
         std::cout << "Error initialising WSA." << std::endl;
         return -1;
     }
 
-    //  (1) Open Socket
-    SOCKET sock0 = socket(AF_INET, SOCK_STREAM, 0);
+    //  (1) ソケットの作成
+    SOCKET sock0 = socket(AF_INET, SOCK_STREAM, 0);     //  socket(アドレスの種類, ソケットのタイプ, プロトコル)
     if (sock0 < 0) {
         perror("socket");
         std::cout << errno << std::endl;
@@ -25,26 +22,27 @@ int main() {
     }
     std::cout << "after scoket" << std::endl;
 
-    //  (2) bind
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(50000);
-    addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(sock0, (struct sockaddr *)&addr, sizeof(addr)) != 0) {
+    //  (2) ソケットのバインド
+    struct sockaddr_in addr;                            //  IPv4のソケットアドレスタイプの構造体
+    addr.sin_family = AF_INET;                          //  アドレスの種類
+    addr.sin_port = htons(50000);                       //  ポート番号
+    addr.sin_addr.s_addr = INADDR_ANY;                  //  アドレス
+    if (bind(sock0, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
         perror("bind");
         return 1;
     }
 
-    //  (3) listen
+    //  (3) クライアントからの接続を待機
     if (listen(sock0, 5) != 0) {
         perror("listen");
         return 1;
     }
 
-    //  (4) accept
+    //  (4) クライアントからの接続を受ける
     while (1) {
+        struct sockaddr_in client;
         int len = sizeof(client);
-        SOCKET sock = accept(sock0, (struct sockaddr *)&client, &len);
+        SOCKET sock = accept(sock0, (struct sockaddr*)&client, &len);
         if (sock < 0) {
             perror("accept");
             break;
@@ -59,7 +57,7 @@ int main() {
         closesocket(sock);
     }
  
-    //  (5) Terminate
+    //  (5) 切断
     closesocket(sock0);
     WSACleanup();
     
