dabceea625232a9adc01e143fcfe12ff9ff8b9de
PANFACTORY <yutatanabe510@gmail.com>
2020-11-28 13:38:56 +0900

    Add httpsclient.cpp

---
+++ httpsclient.cpp
@@ -0,0 +1,93 @@
+//  Link option -lwsock32 -lws2_32 -lssl -lcrypto
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+int main() {
+    std::string hostname = "api.github.com";
+    std::string service = "https";
+    
+    //  (1) winsockの初期化
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) == SOCKET_ERROR) {
+        std::cout << "Error initialising WSA." << std::endl;
+        return -1;
+    }   //  winsockの初期化を行わないとエラー10093が出る
+
+    //  (2) urlから接続情報を取得
+    struct addrinfo hints, *res;
+    struct in_addr addr;
+    int err;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_INET;  //  IPv4:AF_INET, IPv6:AF_INET6, IPv4&IPv6:PF_UNSPEC
+    if ((err = getaddrinfo(hostname.c_str(), service.c_str(), &hints, &res)) != 0) {
+        std::cout << "error " << err << std::endl;
+        return 1;
+    }
+    addr.s_addr= ((struct sockaddr_in *)(res->ai_addr))->sin_addr.s_addr;
+    std::cout << "ip addres: " << inet_ntoa(addr) << std::endl;
+    
+    //  (3) ソケットの作成
+    int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    //  (4) サーバーに接続を要求
+    if (connect(sock, res->ai_addr, res->ai_addrlen) != 0) {
+        perror("connect");
+        return 1;
+    }
+
+    //  (5) SSLに関する設定
+    SSL_load_error_strings();
+    SSL_library_init();
+
+    SSL_CTX *ctx;
+    ctx = SSL_CTX_new(SSLv23_client_method());
+    SSL *ssl;
+    ssl = SSL_new(ctx);
+    err = SSL_set_fd(ssl, sock);
+    SSL_connect(ssl);
+
+    //  (6) サーバーにメッセージを送信
+    char send_buf[256];
+    sprintf(send_buf, "GET /users/PANFACTORY HTTP/1.0\r\n");
+    SSL_write(ssl, send_buf, strlen(send_buf));
+    sprintf(send_buf, "Host: api.github.com\r\n");
+    SSL_write(ssl, send_buf, strlen(send_buf));
+    sprintf(send_buf, "User-Agent:request\r\n");
+    SSL_write(ssl, send_buf, strlen(send_buf));
+    sprintf(send_buf, "\r\n");
+    SSL_write(ssl, send_buf, strlen(send_buf));
+
+    //  (7) サーバーからメッセージを受け取る
+    char buffer[10000];
+    int nDataLength;
+    std::string website_HTML;
+    while ((nDataLength = SSL_read(ssl, buffer, 10000)) > 0) {
+        int i = 0;
+        while (buffer[i] >= 32 || buffer[i] == '\n' || buffer[i] == '\r') {
+            website_HTML += buffer[i];
+            i += 1;
+        }              
+    }
+    std::cout << website_HTML;
+
+    //  (8) 切断
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+    ERR_free_strings();
+    closesocket(sock);
+    freeaddrinfo(res);  // 取得した情報を解放
+    WSACleanup();
+
+    return 0;
+}
